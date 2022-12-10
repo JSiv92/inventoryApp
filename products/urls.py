5c31a933e7b52e6871db098a69ff40482f07e6f3
Jonathan Sivananthan <jonathan1512@gmail.com>
2022-12-10 19:40:38 +1300

    created urlconf for products/

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+"""This file is URLconf. Views are mapped here"""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('index', views.index, name='index'), #http://127.0.0.1:8000/products/index
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+# PRODUCT VIEWS
+
+#http://127.0.0.1:8000/products/index
+def index(request):
+    return HttpResponse("products index.")
+
